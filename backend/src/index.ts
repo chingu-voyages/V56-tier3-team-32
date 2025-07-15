8fa9d2390873ee2f36bb69db01bb8e2d4d09b678
Evaldas Visockas <vievaldas@gmail.com>
2025-07-15 20:49:30 +0300

    V56-18 | Add initial database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,45 @@
+import { MongoClient } from 'mongodb';
 import express from 'express';
+import cors from 'cors';
 
-const app = express();
-const PORT = 3000;
+declare global {
+  namespace Express {
+    interface Request {
+      auth?: {
+        sub: string;
+        [key: string]: any;
+      };
+    }
+  }
+}
 
-app.get('/', (_, res) => {
-  res.send('Hello, what is up!');
-});
+(async () => {
+  const client = new MongoClient(process.env.SURGERY_DATABASE_CONNECTION_CREDENTIALS!);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+  const mongoDb = await connectToMongo();
+
+  async function connectToMongo() {
+    await client.connect();
+    return client.db("testdb");
+  }
+
+  app.get('/', async (_, res) => {
+    const testCollection = mongoDb.collection("testCollection");
+
+    const test = await testCollection.findOne({ name: "Iforgot" });
+
+    res.send(`Surname: ${test!.surname}`);
+  });
+
+
+})();
