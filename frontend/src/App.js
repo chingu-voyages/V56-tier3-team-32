6124da36b8d80effe2194b248d906ab35386fb5b
Evaldas Visockas <vievaldas@gmail.com>
2025-07-22 14:26:52 +0300

    V56-24 | Add frontend auth with Clerk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import StatusList from './components/StatusList';
+import StatusList from './components/StatusList.tsx';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
   return (
-    <>
-     <StatusList/>
-    </>
-   
+    <header>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <StatusList />
+    </header>
   );
 }
 
