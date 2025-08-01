252029b2493976c29d9b512b29f9abd196d951d1
Brandon Isaac <isaac.datch22@students.dkut.ac.ke>
2025-08-01 08:32:17 +0300

    Updated the count of patients per status endpoint

--- backend/src/routes/patientDisplayRoutes.ts
+++ backend/src/routes/patientDisplayRoutes.ts
@@ -1,8 +1,10 @@
 import { getPatientByRecentlyChangedStatus } from "../controllers/patientController";
+import { getPatientsCountByStatus } from "../controllers/patientController";
 import { Router } from 'express';
 
 const router = Router();
 
 router.get("/recents", getPatientByRecentlyChangedStatus);
+router.get("/countbystatus", getPatientsCountByStatus);
 
 export default router;
